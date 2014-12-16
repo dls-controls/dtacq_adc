cb853bc0a023745a94ccce80bdeeb8721f901ea6
Adam Bark <Adam.Bark@diamond.ac.uk>
2014-12-16 17:08:21 +0000

    Removed short circuit return, reindented and removed an old comment from dtacq_adc::readArray.

--- dtacq_adcApp/src/dtacq_adc.cpp
+++ dtacq_adcApp/src/dtacq_adc.cpp
@@ -68,29 +68,27 @@ int dtacq_adc::readArray(int n_samples, int n_channels)
     int status = asynSuccess;
     size_t nread = 0;
     int eomReason, enabled, connected, dType, nBytes, totalRead = 0;
-    pasynManager->isConnected(this->commonDataIPPort, &connected);
-    if (!connected) {
-        return asynError;
-    } else {
+    status = pasynManager->isConnected(this->commonDataIPPort, &connected);
+    if (connected) {
         getIntegerParam(NDDataType, &dType);
         if (dType == NDInt16)
             nBytes = 2;
         else
             nBytes = 4;
-// *2 because normally acquiring 16bits but the carrier allows 32 bit.
         while (totalRead < n_samples * n_channels * nBytes) {
-            status = pasynOctetSyncIO->read(this->octetDataIPPort,
-                                            (char *) this->pRaw->pData + totalRead,
-                                            n_samples*n_channels*nBytes - totalRead,
-                                            5.0, &nread, &eomReason);
+            status = pasynOctetSyncIO->read(
+                this->octetDataIPPort,
+                (char *) this->pRaw->pData + totalRead,
+                n_samples*n_channels*nBytes - totalRead,
+                5.0, &nread, &eomReason);
             if (nread == 0) {
                 printf(this->commonDataIPPort->errorMessage);
                 status = asynError;
                 break;
             }
             totalRead += nread;
-            printf("Still in the loop %d %d %d %d\n", totalRead, nread, eomReason,
-                   n_samples*n_channels*nBytes - totalRead);
+            printf("Still in the loop %d %d %d %d\n", totalRead, nread,
+                   eomReason, n_samples*n_channels*nBytes - totalRead);
         }
         printf("Got %d bytes\n", nread);
         if (status != asynSuccess) {
