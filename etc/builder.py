e286d12661f1a029d1dccdc95d0643fe9737b114
Edmund Warrick <Edmund.Warrick@diamond.ac.uk>
2015-10-30 13:13:10 +0000

    Updated dependencies and fixed template, builder support and example IOC to build against ADCore

--- etc/builder.py
+++ etc/builder.py
@@ -1,25 +1,29 @@
 from iocbuilder import Device, AutoSubstitution, SetSimulation
 from iocbuilder.arginfo import *
+from iocbuilder.modules.asyn import Asyn, AsynPort, AsynIP
 
-from iocbuilder.modules.areaDetector import AreaDetector, _ADBase, _ADBaseTemplate, _NDPluginProducerBase, _NDPluginBase
-from iocbuilder.modules.asyn import AsynIP
+from iocbuilder.modules.ADCore import ADCore, ADBaseTemplate, makeTemplateInstance, includesTemplates
 
+@includesTemplates(ADBaseTemplate)
 class _dtacq_adc(AutoSubstitution):
     TemplateFile = "dtacq_adc.template"
 
-class dtacq_adc(_ADBase):
-    """Creates a simulation detector"""
+class dtacq_adc(AsynPort):
+    """Creates a dtacq_adc driver"""
+    Dependencies = (ADCore, Asyn)
+    UniqueName = "PORT"
     _SpecificTemplate = _dtacq_adc
     def __init__(self, DATA_IP, CONTROL_IP, NCHANNELS=4, NSAMPLES=1000000,
                  BUFFERS=50, MEMORY=0, **args):
         self.controlPort = AsynIP(CONTROL_IP, name = args["PORT"] + ".control")
         # Init the superclass (_ADBase)
-        self.__super.__init__(**args)
+        self.__super.__init__(args["PORT"])
         # Store the args
         self.__dict__.update(locals())
+        makeTemplateInstance(self._SpecificTemplate, locals(), args)
 
     # __init__ arguments
-    ArgInfo = _ADBase.ArgInfo + _SpecificTemplate.ArgInfo + makeArgInfo(
+    ArgInfo = ADBaseTemplate.ArgInfo + _SpecificTemplate.ArgInfo + makeArgInfo(
         __init__,
         DATA_IP = Simple('Address and data port number', str),
         CONTROL_IP = Simple('Address and control port number', str),
@@ -40,6 +44,7 @@ class dtacq_adc(_ADBase):
 '''# dtacq_adcConfig(portName, dataPortName, controlPortName,
 #                 nChannels, nSamples, maxBuffers, maxMemory,
 #                 dataHostInfo)''')
-        print('''dtacq_adcConfig("%(PORT)s", "%(PORT)s.data", \
-"%(PORT)s.control", %(NCHANNELS)d, %(NSAMPLES)d, \
-%(BUFFERS)d, %(MEMORY)d, "%(DATA_IP)s")''' % self.__dict__)
+        print('''dtacq_adcConfig("%s", "%s.data", \
+"%s.control", %d, %d, \
+%d, %d, "%s")''' % (self.__dict__["args"]["PORT"], self.__dict__["args"]["PORT"], self.__dict__["args"]["PORT"], self.__dict__["NCHANNELS"], self.__dict__["NSAMPLES"], self.__dict__["BUFFERS"], self.__dict__["MEMORY"], self.__dict__["DATA_IP"]))
+
